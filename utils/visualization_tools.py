782a88f9088e5468033f81fb1df837a956568973
kondratevakate <marinapoms@gmail.com>
2021-01-11 15:13:13 +0300

    new fuction

--- utils/visualization_tools.py
+++ utils/visualization_tools.py
@@ -24,17 +24,31 @@ def plot_central_cuts(img):
         img = img.get_fdata()
    
     fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(3 * 4, 4))
+    fig.suptitle('Central cuts', fontsize=16)
     
-    axes[0].imshow(img[ img.shape[0] // 2, :, :])
-    axes[1].imshow(img[ :, img.shape[1] // 2, :])
-    axes[2].imshow(img[ :, :, img.shape[2] // 2])
+    axes[0].imshow(img[ img.shape[0] // 2, :, :], cmap = 'gray')
+    axes[0].set_title(f'coordinate sagital = {img.shape[0] // 2}')
+    axes[1].imshow(img[ :, img.shape[1] // 2, :], cmap = 'gray')
+    axes[1].set_title(f'coordinate coronal = {img.shape[1] // 2}')
+    axes[2].imshow(img[ :, :, img.shape[2] // 2], cmap = 'gray')
+    axes[2].set_title(f'coordinate axial = {img.shape[2] // 2}')
     
     plt.show()
 
-def plot_certain_cuts(img, coordinates):
+def plot_certain_cuts(img, coordinates, object_):
+    
     """
-    param image: tensor or np array of shape (CxDxHxW) if t is None
+    Function plots central slices of MRI
+    
+    Arguments:
+        * img (torch.Tensor): MR image (1xDxHxW)
+        * coordinates (x, y, z): coordinates of slices which we want to see
+        * object_ (str): name of object for which we plot slices, e.g 'brain' or 'bb'
+    
+    Output:
+        * picture of certain slices of MRI
     """
+    
     if isinstance(img, torch.Tensor):
         img = img.numpy()
         if (len(img.shape) > 3):
@@ -42,11 +56,57 @@ def plot_certain_cuts(img, coordinates):
                 
     elif isinstance(img, nibabel.nifti1.Nifti1Image):    
         img = img.get_fdata()
-   
+    
     coordinate_sagital, coordinate_coronal, coordinate_axial = coordinates
     fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(3 * 4, 4))
-    axes[0].imshow(img[ coordinate_sagital, :, :])
-    axes[1].imshow(img[ :, coordinate_coronal, :])
-    axes[2].imshow(img[ :, :, coordinate_axial])
+    fig.suptitle(f'Certain cuts of {object_}', fontsize=16)
+    
+    axes[0].imshow(img[ coordinate_sagital, :, :], cmap = 'gray')
+    axes[0].set_title(f'coordinate sagital = {coordinate_sagital}')
+    axes[1].imshow(img[ :, coordinate_coronal, :], cmap = 'gray')
+    axes[1].set_title(f'coordinate coronal = {coordinate_coronal}')
+    axes[2].imshow(img[ :, :, coordinate_axial], cmap = 'gray')
+    axes[2].set_title(f'coordinate axial = {coordinate_axial}')
     
     plt.show()
+    
+def get_center_coord_of_bb(img):
+    
+    '''
+    Function finds central coordinates of bb
+    
+    Arguments:
+        * img (torch.Tensor): MR image (1xDxHxW)
+        
+    Output:
+        * sagital_center_coord, coronal_center_coord, axial_center_coord (int): corresponding central   coordinates of bb 
+    '''
+    
+    if isinstance(img, torch.Tensor):
+        img = img.numpy()
+        if (len(img.shape) > 3):
+            img = img[0,:,:,:]
+                
+    elif isinstance(img, nibabel.nifti1.Nifti1Image):    
+        img = img.get_fdata()
+    
+    axial_coord = []
+    for i in range(img.shape[2]):
+        if (img[:, :, i] != 0).any():
+            axial_coord.append(i)
+    axial_center_coord = (min(axial_coord)+max(axial_coord))//2 
+    
+    coronal_coord = []
+    for i in range(img.shape[1]):
+        if (img[:, i, :] != 0).any():
+            coronal_coord.append(i)
+    coronal_center_coord = (min(coronal_coord)+max(coronal_coord))//2 
+    
+    sagital_coord = []
+    for i in range(img.shape[0]):
+        if (img[i, :, :] != 0).any():
+            sagital_coord.append(i)
+    sagital_center_coord = (min(sagital_coord)+max(sagital_coord))//2 
+
+                        
+    return sagital_center_coord, coronal_center_coord, axial_center_coord
